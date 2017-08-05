6767c15a7f04f87fc51f33bd33fcdcb62af2a4a8
Rawshan008 <pust.love008@gmail.com>
2017-08-05 22:18:29 +0600

    upload

---
+++ assets/js/active.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+ 
+  $(".home-page-slider").owlCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+      items : 1,
+      navigation : true,
+      pagination:false,
+ 
+  });
+ 
+});
